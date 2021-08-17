a65a0dd471f84f5499af5309dbcec698e2ee6d0b
kyawe1 <kk09254010314@gmail.com>
2021-08-18 06:23:39 +0630

    final

--- group sample/button.js
+++ group sample/button.js
@@ -2,24 +2,23 @@ m = document.getElementsByClassName('btn btn-primary w-100')
 for (i of m) {
 
     i.onclick = (e) => {
-        if (e.target.tagName == 'i' || !e.target.toElement) {
-            return;
+        
+        if (e.target.tagName == 'BUTTON') {
+            // console.log(e)
+            // console.log(e.target.children[0].className)
+            if (e.target.children[0].className == 'bi bi-star-fill mx-2') {
+                e.target.children[0].className = 'bi bi-star mx-2'
+            } else {
+                e.target.children[0].className = 'bi bi-star-fill mx-2'
+            }
         }
-        console.log(e)
-        console.log(e.target.children[0].className)
-        if (e.target.children[0].className == 'bi bi-star-fill mx-2') {
-            e.target.children[0].className = 'bi bi-star mx-2'
-        } else {
-            e.target.children[0].className = 'bi bi-star-fill mx-2'
-        }
+        
 
     }
 }
 k = document.getElementsByName('btn-icon')
 for (i of k) {
     i.onclick = (e) => {
-        console.log(e)
-        console.log(e.target.className)
         if (e.target.className == 'bi bi-star-fill mx-2') {
             e.target.className = 'bi bi-star mx-2'
         } else {
